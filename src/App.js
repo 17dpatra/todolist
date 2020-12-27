00b421efeace7621227a3b8f7db4f8298a262ad1
17dpatra <devy.patra@gmail.com>
2020-12-26 20:18:25 -0500

    basic stencil of input box, creating handler for input change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
-import React from "react";
-import './Form.js';
+import React, {useState} from "react";
+import Form from './Form.js';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  //takes a todo and adds it to the beginning of list of todos
+  function add(todo) {
+    setTodos([todo, ...todos]); //adds new todo to the beginning of array, the rest is old todos
+  }
+
   return (
     <div className="App">
       <h1>Add to your TodoList!</h1>
-      <Form />
+      <Form add={add}/>
     </div>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -1,25 +1,31 @@
-import React, {useState} from "react";
+import React, {Component} from "react";
 
 //keeps track of our todo state through a form
-const Form = () => {
-    const [todo, setTodo] = useState({
-        id: "",
-        task: "", //text describing the todo
-        completed: false //keeps track of whether the task has been completed
-    });
+export default class Form extends Component {
 
-    //handles when user types a todo to keep track of it
-    function handleInputChange(e) {
-        //pass new object with old todo property and update task with user's input
-        setTodo({...todo, task: e.target.value});
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            task:"",
+            totalList: []
+        }
+    }
+    
+    handleInputChange(e) {
+        
     }
 
-    return (
-        <form>
-            <input name="task" type="text" onChange={handleInputChange} value={todo.task}/>
-            <button />
-        </form>
-    );
+    render() {
+        return (
+            <form>
+                <input 
+                name="task" 
+                type="text" 
+                onChange={(event) => this.handleInputChange(event.target.value)} 
+                value={this.state.task}/>
+                <button />
+            </form>
+        );
+    }
 }
-
-export default Form;
